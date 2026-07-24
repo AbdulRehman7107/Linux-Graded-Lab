47f42508be0a85ec8e8d1f08795ccad4ae8e0d8d
Abdul Rehman Altaf Mulla <mullaabdulrehman57@gmail.com>
2026-07-24 23:38:02 +0530

    Implement backup script with duplicate detection
    The script processes files in the 'submissions' directory, creating a backup in the 'backup' directory while logging duplicates and errors.

--- Question1/duplicate_backup.sh
+++ Question1/duplicate_backup.sh
@@ -1,1 +1,42 @@
+#!/bin/bash
 
+SOURCE="submissions"
+BACKUP="backup"
+
+mkdir -p "$BACKUP"
+
+REPORT="report.txt"
+ERROR_LOG="errors.log"
+
+processed=0
+duplicates=0
+backedup=0
+
+> "$REPORT"
+> "$ERROR_LOG"
+
+declare -A filehash
+
+for file in "$SOURCE"/*
+do
+    if [ -f "$file" ]; then
+        processed=$((processed + 1))
+
+        hash=$(md5sum "$file" 2>>"$ERROR_LOG" | cut -d' ' -f1)
+
+        if [[ -z "${filehash[$hash]}" ]]; then
+            filehash[$hash]=$file
+            cp "$file" "$BACKUP"/ 2>>"$ERROR_LOG"
+            backedup=$((backedup + 1))
+        else
+            duplicates=$((duplicates + 1))
+        fi
+    fi
+done
+
+echo "========== Submission Report ==========" >> "$REPORT"
+echo "Files Processed : $processed" >> "$REPORT"
+echo "Duplicate Files : $duplicates" >> "$REPORT"
+echo "Files Backed Up : $backedup" >> "$REPORT"
+
+echo "Task Completed."
